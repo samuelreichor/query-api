57958734e377a9064d9c2bf3764a9b867766aa08
samuelreichor <samuelreichor@gmail.com>
2025-10-18 17:02:24 +0200

    chore(next-app): Update relatedNews component

--- playgrounds/next-app/src/components/RelatedNews.tsx
+++ playgrounds/next-app/src/components/RelatedNews.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { useCraftUri, useCraftEntry, useCraftCurrentSite } from '@query-api/next'
+import { useCraftEntry, useCraftCurrentSite } from '@query-api/next'
 
 export default function RelatedNews() {
-  const uri = useCraftUri()
   const { id } = useCraftCurrentSite()
-  const { data, loading, error } = useCraftEntry().uri(uri).siteId(id).one()
+  const { data, loading, error } = useCraftEntry().id([431, 1527, 331, 383]).siteId(id).all()
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {error}</p>
